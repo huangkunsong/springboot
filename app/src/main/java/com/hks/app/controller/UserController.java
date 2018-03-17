ae1b9bd09770ded5577674b27e5b18928c4738bb
huangkunsong <huang_kunsong@sina.cn>
2018-03-17 14:48:37 +0800

    ++++++++++++++++++++++

--- app/src/main/java/com/hks/app/controller/UserController.java
+++ app/src/main/java/com/hks/app/controller/UserController.java
@@ -1,36 +1,31 @@
 package com.hks.app.controller;
 
 import com.hks.app.dao.ITestDao;
-import com.hks.app.entity.Contact;
 import com.hks.app.entity.User;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+@Api(value = "用户账户操作API", description = "用户账户操作API")
 @RestController
 @RequestMapping("/")
-public class ContactController {
+public class UserController {
 
     @Autowired
     ITestDao iTestDao;
 
 	@RequestMapping(method=RequestMethod.GET)
 	public String home(Map<String,Object> model) {
-        List<Contact> contacts = new ArrayList<>();
-        for (int i = 0; i < 5; i++) {
-            Contact contact = new Contact();
-            contact.setEmailAddress("Address" + i);
-            contact.setFirstName("asdasdasd" + "i");
-        }
-        model.put("contacts", contacts);
         return "home";
 	}
 
+	@ApiOperation(value = "获取所有用户", notes = "根据用户获取所有")
     @RequestMapping(value = "/home", method = RequestMethod.GET)
     public List<User> home(User user){
         return iTestDao.findAllUser(user);
