0c39f7c2fe8e345e952067c885a24f16a104a0b7
huangkunsong <huang_kunsong@sina.cn>
2018-08-26 23:08:27 +0800

    ++++++++++++++++++++++

--- service-consumer/src/main/java/com/hks/consumer/controller/UserController.java
+++ service-consumer/src/main/java/com/hks/consumer/controller/UserController.java
@@ -3,14 +3,20 @@ package com.hks.consumer.controller;
 import com.hks.consumer.entity.UserVO;
 import com.hks.consumer.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RefreshScope
 @RestController
 @RequestMapping(value = "/consumer" , produces = MediaType.APPLICATION_JSON_VALUE)
 public class UserController {
+
+    @Value(value = "${info.a}")
+    private String value;
 
     @Autowired
     UserService userService;
@@ -23,6 +29,11 @@ public class UserController {
     @RequestMapping(path = "/findUser/{userId}")
     public UserVO getUserByUserName(@PathVariable String userId) {
         return userService.findUserById(userId);
+    }
+
+    @RequestMapping(path = "/getValue")
+    public String getValue() {
+        return this.value;
     }
 
 }
